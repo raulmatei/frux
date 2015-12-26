a1277577f1e2572725ef338501814bbac4a8ab9a
Raul Matei <raulmatei@gmail.com>
2015-12-26 18:36:41 +0200

    Create module should not expose babel properties

--- src/create-module.js
+++ src/create-module.js
@@ -1,4 +1,7 @@
+import pick from 'lodash/object/pick';
 import isEmpty from 'lodash/lang/isEmpty';
+import isArray from 'lodash/lang/isArray';
+import isFunction from 'lodash/lang/isFunction';
 
 export default function createModule({ stores, actions, getters }) {
   return (name, target, reactor) => {
@@ -6,7 +9,7 @@ export default function createModule({ stores, actions, getters }) {
       reactor.registerStores(stores);
     }
 
-    target.actions[name] = actions;
-    target.getters[name] = getters;
+    target.actions[name] = pick(actions, isFunction);
+    target.getters[name] = pick(getters, isArray);
   };
 }
