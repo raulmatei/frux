5a4160fabfd631fa03966f7bb9ce0a554285f2cf
Raul Matei <raulmatei@gmail.com>
2016-04-23 10:44:18 +0300

    Remove `examples/` directory

--- examples/calculator/modules/operations/action-types.js
+++
@@ -1,5 +0,0 @@
-export const SEND_OPERATOR = 'SEND_OPERATOR';
-export const INPUT_NUMBER = 'INPUT_NUMBER';
-export const DELETE_LAST = 'DELETE_LAST';
-export const COMPUTE_RESULT = 'COMPUTE_RESULT';
-export const CLEAR = 'CLEAR';
--- examples/counter/application-context.js
+++
@@ -1,7 +0,0 @@
-import frux from 'frux';
-import counter from './modules/counter';
-
-export const { actions, getters } = frux.initialize({
-  options: { debug: true },
-  counter
-});
