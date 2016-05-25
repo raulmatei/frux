7188d27749cc932c7098fe4648213e546f520050
Raul Matei <raulmatei@gmail.com>
2016-05-25 23:07:48 +0300

    Make sure functional getters are added to the getters registry

--- src/create-module.js
+++ src/create-module.js
@@ -4,12 +4,14 @@ import isArray from 'lodash/isArray';
 import isFunction from 'lodash/isFunction';
 
 export default function createModule({ stores, actions, getters }) {
+  const looksLikeGetter = (value) => isFunction(value) || isArray(value);
+
   return (name, target, reactor) => {
     if (!isEmpty(stores)) {
       reactor.registerStores(stores);
     }
 
     target.actions[name] = pickBy(actions, isFunction);
-    target.getters[name] = pickBy(getters, isArray);
+    target.getters[name] = pickBy(getters, looksLikeGetter);
   };
 }
