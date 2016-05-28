a51a92cad17c439a6c61819aec234b1543111e1e
Raul Matei <raulmatei@gmail.com>
2016-05-28 14:41:06 +0300

    Test && refactor `createModule`

--- src/create-module.js
+++ src/create-module.js
@@ -1,12 +1,22 @@
 import pickBy from 'lodash/pickBy';
 import isEmpty from 'lodash/isEmpty';
 import isArray from 'lodash/isArray';
+import isObject from 'lodash/isObject';
 import isFunction from 'lodash/isFunction';
 
-export default function createModule({ stores, actions, getters }) {
+export default function createModule(descriptor) {
   const looksLikeGetter = (value) => isFunction(value) || isArray(value);
 
+  if (!descriptor || (!isObject(descriptor) || isArray(descriptor))) {
+    throw new Error(
+      'createModule() requires a descriptor object ' +
+      '`{ stores, actions, getters }` to be passed as an argument'
+    );
+  }
+
   return (name, target, reactor) => {
+    const { stores = {}, actions = {}, getters = {} } = descriptor;
+
     if (!isEmpty(stores)) {
       reactor.registerStores(stores);
     }
