cd0378e54d43c5042f3b3f8273334720d8b89bc6
Raul Matei <raulmatei@gmail.com>
2016-05-29 01:05:13 +0300

    Update create-mount-node-test

--- test/create-mount-node-test.js
+++ test/create-mount-node-test.js
@@ -2,12 +2,15 @@ import test from 'ava';
 import createMountNode from '../src/create-mount-node';
 import document from 'global/document';
 
-global.document = document;
 
 test('createMountNode()', (assert) => {
+  global.document = document;
+
   const node = createMountNode();
+  const parent = node.parentNode;
 
   assert.is(node.tagName, 'DIV', 'should create a `div` element');
-  assert.is(node.parentNode.tagName, 'BODY', 'should be created inside `body` tag');
   assert.deepEqual(node.childNodes, [], 'should create an empty `div` element');
+  assert.is(parent.tagName, 'BODY', 'should be created inside `body` tag');
+  assert.deepEqual(parent.childNodes[0], node, 'should be created as the first element in `body` tag');
 });
